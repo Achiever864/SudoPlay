0714e2aec5896c981d87dc49a3c9e703d7940bdf
Achiever864 <ikhuemoisaigaga@gmail.com>
2026-07-13 09:43:22 +0100

    Authorization Middleware completed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -4,6 +4,7 @@
 // get Profile
 import User from "../models/User.model.js";
 import jwt from "jsonwebtoken";
+import bcryptjs from "'bcryptjs";
 
 //helper function
 const generateGuestUsername = async () => {
@@ -92,8 +93,137 @@ const registerGuestUer = async(req, res) => {
             });
         }
 
-        const hashedPassword = await bcrypt.hash(password, 10)
+        const hashedPassword = await bcrypt.hash(password, 10);
+        user.username = username;
+        user.email = email.toLowerCase();
+        user.password = hashedPassword;
+        user.isGuest = false;
+
+        await user.save();
+
+        //generate fresh JWT
+        const token = jwt.sign(
+            {
+                id: User._id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "15d"
+            }
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: "Account created successfully",
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                isGuest: user.isGuest
+            }
+        })
     } catch (error) {
-        
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Unable to register user"
+        })
     }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "Please provide email and password."
+            });
+        }
+
+        const user = await User.findOne({
+            email: email.toLowerCase()
+        })
+
+        if (!user){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid email or password"
+            });
+        }
+
+        //compare passwords
+        const isMatch = await bcrypt.compare(
+            passsword,
+            user.password
+        );
+
+        if(!isMatch){
+            return res.status(401).json({
+               success: false,
+               message: "Invalid email or password."
+            });
+        }
+
+        //the JWT
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            process.env.JWT_SECRET,
+            {
+            expiresIn: "15d"
+            }
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: "Login successful.",
+            token,
+            user: {
+                id: user._id,
+                username: user.username,
+                email: user.email,
+                isGuest: user.isGuest,
+                avatar: user.avatar,
+            }
+        })
+    } catch (error){
+        return res.status(500).json({
+            success: false,
+            message: "Unable to login!"
+        })
+    }
+}
+
+const getProfile = async(req, res) => {
+    try {
+        const user = await User.findById(req.user.id)
+            .select("-password");
+        
+        if (!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            user
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Unable to get user profile!"
+        })
+    }
+};
+
+export {
+    registerGuestUer,
+    createGuestUser,
+    loginUser,
+    getProfile
 }
